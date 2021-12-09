6b097743e85952a712424444ea235e7fe04c8a30
Trevor <tjameson15@gmail.com>
2021-12-09 11:49:58 -0500

    Finished day 1 part 2.

--- day1.py
+++ day1.py
@@ -8,8 +8,22 @@ def depth_finder(input):
     for i, d in enumerate(depths):
         if i != 0 and int(depths[i - 1]) < int(d): 
             sink_count += 1
-    return sink_count
+    print('depth_finder results: ', sink_count)
     
 
 depth_finder(input)
 
+def depth_finder_2_the_deepening(input):
+    depths = input.splitlines()
+    dep_len = len(depths)
+    sink_count = 0
+    for i in range(0, dep_len):
+        if (i < dep_len - 3):
+            seg1 = int(depths[i]) + int(depths[i + 1]) + int(depths[i + 2])
+            seg2 = int(depths[i + 1]) + int(depths[i + 2]) + int(depths[i + 3])
+            if (seg1 < seg2):
+                sink_count += 1
+        
+    print('depth_finder_2_the_deepening results', sink_count)
+
+depth_finder_2_the_deepening(input)
