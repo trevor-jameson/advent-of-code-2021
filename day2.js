8d0b109b4b5b1484cc8fd0aaa5c6a50928172392
Trevor <tjameson15@gmail.com>
2021-12-09 12:28:42 -0500

    Finished day 1 part 2.

--- day2.js
+++ day2.js
@@ -17,4 +17,31 @@ function dive() {
         console.log(position);
     })
 }
+
 dive()
+
+function diveStraight() {
+    fs.readFile('./day2_input.txt', 'utf8', function(err, data){
+        if (err) console.log('You failed at Christmas :(')
+        const instructions = data.split('\n')
+        let horizontal = 0;
+        let depth = 0;
+        let aim = 0;
+        instructions.forEach(inst => {
+            let [direction, units] = inst.split(' ');
+            units = Number(units)
+            if (direction === 'forward') {
+                horizontal += units;
+                depth += (aim * units);
+            } else if (direction === 'down') {
+                aim += units;
+            } else if (direction === 'up') {
+                aim -= units;
+            };
+        })
+        const position = depth * horizontal;
+        console.log(position);
+    })
+}
+
+diveStraight()
